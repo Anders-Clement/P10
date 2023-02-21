e69b04c8567184c448828580e9f41af027f9a192
Anders Clement <andersclement96@gmail.com>
2023-02-21 16:00:40 +0100

    Added listening to robot_state_transition_event to change color

--- src/spice/scripts/led_node.py
+++ src/spice/scripts/led_node.py
@@ -3,12 +3,19 @@
 import rclpy
 from rclpy.node import Node
 
-from spice_msgs.msg import LedOutput
+from spice_msgs.msg import LedOutput, RobotStateTransition, RobotState, LedOutput
 
 import pigpio
 
-class gpio():
-    def __init__(self) -> None:
+class LedNode(Node):
+    def __init__(self):
+        super().__init__('led_node')
+        self.led_sub = self.create_subscription(LedOutput, 'led_output', self.led_cb, 10)
+        self.robot_state_transition_event_sub = self.create_subscription(
+            RobotStateTransition,
+            'robot_state_transition_event',
+            self.robot_state_transition_event_cb,
+            10)
         self.ledPins = {'r': 26,
                         'g': 13,
                         'b': 19}
@@ -17,33 +24,37 @@ class gpio():
             self.pi.set_PWM_frequency(pin, 8000)
             self.pi.set_PWM_dutycycle(pin, 0)
 
-    def set_rgb(self, output: LedOutput):
-        self.pi.set_PWM_dutycycle(self.ledPins['r'], output.red*25)
-        self.pi.set_PWM_dutycycle(self.ledPins['g'], output.green*25)
-        self.pi.set_PWM_dutycycle(self.ledPins['b'], output.blue*25)
-
-class MinimalPublisher(Node):
-
-    def __init__(self):
-        super().__init__('led_node')
-        self.led_sub = self.create_subscription(LedOutput, 'led_output', self.led_cb, 10)
-        self.gpio = gpio()
+    def robot_state_transition_event_cb(self, msg: RobotStateTransition) -> None:
+        if msg.new_state.state == RobotState.STARTUP:
+            self.led_cb(LedOutput(red=True, blue=False, green=True))
+        elif msg.new_state.state == RobotState.READY_FOR_JOB:
+            self.led_cb(LedOutput(red=False, blue=False, green=True))
+        elif msg.new_state.state == RobotState.MOVING:
+            self.led_cb(LedOutput(red=False, blue=True, green=True))
+        elif msg.new_state.state == RobotState.PROCESSING:
+            self.led_cb(LedOutput(red=True, blue=True, green=True))
+        elif msg.new_state.state == RobotState.ERROR:
+            self.led_cb(LedOutput(red=True, blue=False, green=False))
+        
 
     def led_cb(self, msg: LedOutput):
         self.get_logger().info(f'Setting LEDs: \n{msg}')
-        self.gpio.set_rgb(msg)
+        self.pi.set_PWM_dutycycle(self.ledPins['r'], msg.red*25)
+        self.pi.set_PWM_dutycycle(self.ledPins['g'], msg.green*25)
+        self.pi.set_PWM_dutycycle(self.ledPins['b'], msg.blue*25)
+
 
 def main(args=None):
     rclpy.init(args=args)
 
-    minimal_publisher = MinimalPublisher()
+    led_node = LedNode()
 
-    rclpy.spin(minimal_publisher)
+    rclpy.spin(led_node)
 
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
-    minimal_publisher.destroy_node()
+    led_node.destroy_node()
     rclpy.shutdown()
 
 
