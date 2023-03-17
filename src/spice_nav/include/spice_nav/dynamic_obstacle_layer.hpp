60f97c3dbf5410463d0873b1a4237451826a13ba
Olibeyer <olauri18@student.aau.dk>
2023-03-17 14:41:18 +0100

    dynamic_obstacle_layer plugin no python script

--- src/spice_nav/include/spice_nav/dynamic_obstacle_layer.hpp
+++ src/spice_nav/include/spice_nav/dynamic_obstacle_layer.hpp
@@ -1,6 +1,6 @@
 #ifndef DYNAMIC_OBSTACLE_LAYER_HPP
 #define DYNAMIC_OBSTACLE_LAYER_HPP
-
+#include "math.h"
 #include "map"
 #include "string"
 #include "vector"
@@ -9,8 +9,8 @@
 #include "nav2_costmap_2d/layered_costmap.hpp"
 #include "geometry_msgs/msg/pose_array.hpp"
 #include "tf2/exceptions.h"
-#include "tf2_ros/transform_listener.h"
-#include "tf2_ros/buffer.h"
+#include "tf2_msgs/msg/tf_message.hpp"
+#include "spice_msgs/srv/get_robots_by_type.hpp"
 
 namespace nav2_costmap_2d
 {
@@ -40,17 +40,24 @@ public:
   }
   virtual void matchSize();
 
-  virtual void DynamicObstacleCallback(geometry_msgs::msg::PoseArray::SharedPtr msg);
+  virtual void TFCallback(tf2_msgs::msg::TFMessage::SharedPtr msg);
+
+  virtual void get_robots_on_timer_cb();
 
 private:
     double last_min_x_, last_min_y_, last_max_x_, last_max_y_;
+    float obstacle_points_;
     float update_;
     std::string topic_;
-    rclcpp::Subscription<geometry_msgs::msg::PoseArray>::SharedPtr subscription_;
-    std::map<std::string, geometry_msgs::msg::PoseArray> messageBuffer;
+    rclcpp::Subscription<tf2_msgs::msg::TFMessage>::SharedPtr subscription_;
+    std::map<std::string, geometry_msgs::msg::TransformStamped> messageBuffer;
     rclcpp_lifecycle::LifecycleNode::SharedPtr nh_;
-    std::shared_ptr<tf2_ros::TransformListener> tf_listener_{nullptr};
-   std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
+    rclcpp::TimerBase::SharedPtr timer_{nullptr};
+    rclcpp::Client<spice_msgs::srv::GetRobotsByType>::SharedPtr get_robots_cli;
+    std::vector<spice_msgs::msg::Robot> robot_list; //list of all robots including workcell
+    std::string robot_name;
+    double ROBOT_RADIUS = 0.15;
+    double ANGLE_INCREMENT;
 };
 }  // namespace nav2_costmap_2d
 #endif
