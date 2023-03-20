198d313c7e5d7d823fe6ac78cae6274970a05177
Anders Clement <andersclement96@gmail.com>
2023-03-20 13:52:28 +0100

    tf remapping

--- src/spice_nav/launch/nav_bringup.launch.py
+++ src/spice_nav/launch/nav_bringup.launch.py
@@ -52,6 +52,7 @@ def generate_launch_description():
     #              https://github.com/ros2/launch_ros/issues/56
     remappings = [('/tf', 'tf'),
                   ('/tf_static', 'tf_static'),
+                  ('/global_tf', '/tf'),
                   ('/scan', ['/',LaunchConfiguration('namespace'),'/scan'])]
 
     # Create our own temporary YAML files that include substitutions
