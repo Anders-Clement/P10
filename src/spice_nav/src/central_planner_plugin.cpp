b08818ee0f2b7986477ce54ed26fd53ac3bca2ba
Anders Clement <andersclement96@gmail.com>
2023-03-21 10:36:29 +0100

    Debug local planning

--- src/spice_nav/src/central_planner_plugin.cpp
+++ src/spice_nav/src/central_planner_plugin.cpp
@@ -104,12 +104,63 @@ nav_msgs::msg::Path CentralPlanner::createPlan(
     request->start = start;
     request->goal = goal;
     request->tolerance = goal_tolerance_;
-    auto future = central_planner_client->async_send_request(request);
-    RCLCPP_INFO(node_->get_logger(), "Send planning request");
-    rclcpp::spin_until_future_complete(node_, future);
-    auto response = future.get();
+    // auto future = central_planner_client->async_send_request(request);
+    // RCLCPP_INFO(node_->get_logger(), "Send planning request");
+    // rclcpp::spin_until_future_complete(node_, future);
+    // auto response = future.get();
+    auto response = debug_straight_line_planner(request);
     RCLCPP_INFO(node_->get_logger(), "Got response path with %d poses", response->plan.poses.size());
     return response->plan;
+}
+
+nav_msgs::srv::GetPlan::Response::SharedPtr CentralPlanner::debug_straight_line_planner(nav_msgs::srv::GetPlan::Request::SharedPtr request)
+{
+  nav_msgs::srv::GetPlan::Response::SharedPtr response = std::make_shared<nav_msgs::srv::GetPlan::Response>();
+  // Checking if the goal and start state is in the global frame
+  if (request->start.header.frame_id != global_frame_) {
+      RCLCPP_ERROR(
+          node_->get_logger(), "Planner will only except start position from %s frame",
+          global_frame_.c_str());
+      return;
+  }
+
+  if (request->goal.header.frame_id != global_frame_) {
+      RCLCPP_INFO(
+          node_->get_logger(), "Planner will only except goal position from %s frame",
+          global_frame_.c_str());
+      return;
+  }
+
+  double interpolation_resolution = 0.05;//request->tolerance;
+
+  response->plan.poses.clear();
+  response->plan.header.stamp = node_->now();
+  response->plan.header.frame_id = global_frame_;
+  // calculating the number of loops for current value of interpolation_resolution_
+  int total_number_of_loop = std::hypot(
+      request->goal.pose.position.x - request->start.pose.position.x,
+      request->goal.pose.position.y - request->start.pose.position.y) /
+      interpolation_resolution;
+  double x_increment = (request->goal.pose.position.x - request->start.pose.position.x) / total_number_of_loop;
+  double y_increment = (request->goal.pose.position.y - request->start.pose.position.y) / total_number_of_loop;
+
+  for (int i = 0; i < total_number_of_loop; ++i) {
+      geometry_msgs::msg::PoseStamped pose;
+      pose.pose.position.x = request->start.pose.position.x + x_increment * i;
+      pose.pose.position.y = request->start.pose.position.y + y_increment * i;
+      pose.pose.position.z = 0.0;
+      pose.pose.orientation.x = 0.0;
+      pose.pose.orientation.y = 0.0;
+      pose.pose.orientation.z = 0.0;
+      pose.pose.orientation.w = 1.0;
+      pose.header.stamp = node_->now();
+      pose.header.frame_id = global_frame_;
+      response->plan.poses.push_back(pose);
+  }
+
+  response->plan.poses.push_back(request->goal);
+  RCLCPP_INFO(node_->get_logger(), "Created a plan with %d poses", response->plan.poses.size());
+  return response;
 }
 
 }  // namespace spice_nav
