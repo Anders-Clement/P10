1c8031fec4fa2a8ad0bb4bc6b65e892bc667b51f
Anders Clement <andersclement96@gmail.com>
2023-03-24 09:44:48 +0100

    Added storage for previously calculated plans

--- src/spice/src/central_path_planner.cpp
+++ src/spice/src/central_path_planner.cpp
@@ -38,12 +38,19 @@ void CentralPathPlanner::get_plan_cb(
     response->plan = m_planner->get_plan(request->start, request->goal, m_tolerance, request->id);
     auto duration = (now()-start_time).nanoseconds()*10e-9;
 
+    m_planned_paths[request->id.id] = response->plan;
+
     RCLCPP_INFO(get_logger(), "Created a plan with %ld poses in %f ms", response->plan.poses.size(), duration);
 }
 
 std::shared_ptr<nav2_costmap_2d::Costmap2D> CentralPathPlanner::get_costmap(spice_msgs::msg::Id id)
 {
     return m_costmap->get_costmap(id);
+}
+
+nav_msgs::msg::Path& CentralPathPlanner::get_last_plan_by_id(spice_msgs::msg::Id id)
+{
+    return m_planned_paths[id.id];
 }
 
 
