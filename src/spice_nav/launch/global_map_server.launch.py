42b3500dc3e07a68e47e817aaa69d928d8c41a62
Jonas Andersen <jan18@student.aau.dk>
2023-03-24 10:37:25 +0100

    Changed so no lifecycle manager is used and just activates 'manually' instead

--- src/spice_nav/launch/global_map_server.launch.py
+++ src/spice_nav/launch/global_map_server.launch.py
@@ -2,26 +2,19 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
-from launch_ros.actions import Node
 from nav2_common.launch import RewrittenYaml
 from launch_ros.actions import LifecycleNode
+from launch.substitutions import FindExecutable
+from launch.actions import ExecuteProcess
 
-# from launch.actions import EmitEvent
-# from launch_ros.events.lifecycle import ChangeState
-# from launch_ros.event_handlers import OnStateTransition
-
-# import launch
-# import lifecycle_msgs.msg
 
 MAP_NAME = 'C4.yaml' # Change name of map here
 
 def generate_launch_description():
 
 
-    use_sim_time = LaunchConfiguration('use_sim_time')
     use_respawn = LaunchConfiguration('use_respawn')
     log_level = LaunchConfiguration('log_level')
-    autostart = LaunchConfiguration('autostart')
 
 
         
@@ -35,7 +28,7 @@ def generate_launch_description():
          'maps', MAP_NAME]
     )
 
-    # Create our own temporary YAML files that include substitutions
+    ## Create our own temporary YAML files that include substitutions
     param_substitutions = {
         'yaml_filename': map_path}
 
@@ -44,6 +37,7 @@ def generate_launch_description():
             param_rewrites=param_substitutions,
             convert_types=True)
     
+    ## Launch Arguements
     declare_use_respawn_cmd = DeclareLaunchArgument(
         name='use_respawn',
         default_value='False',
@@ -53,19 +47,8 @@ def generate_launch_description():
         name='log_level',
         default_value='info',
         description='log level')
-    
-    declare_use_sim_time_cmd = DeclareLaunchArgument(
-        name='use_sim_time',
-        default_value='false',
-        description='Enable use_sime_time to true'
-    )
 
-    declare_autostart_cmd = DeclareLaunchArgument(
-        name='autostart',
-        default_value='true',
-        description='Automatically startup the nav2 stack')
-
-
+    ## Nodes
     map_server_node = LifecycleNode(
         package='nav2_map_server',
         executable='map_server',
@@ -86,52 +69,50 @@ def generate_launch_description():
         parameters=[configured_params],
         arguments=['--ros-args', '--log-level', log_level]
     )
-    
+
     return LaunchDescription([
         declare_use_respawn_cmd,
-        declare_log_level_cmd,
-        declare_use_sim_time_cmd,
-        declare_autostart_cmd,
+        declare_log_level_cmd, 
+    
         map_server_node,
         costmap_node,
 
-        # EmitEvent(
-        #     event = ChangeState(
-        #     lifecycle_node_matcher = launch.events.process.matches_name('global_map_server'),
-        #     transition_id = lifecycle_msgs.msg.Transition.TRANSITION_CONFIGURE,
-        #     )
-        # ),
 
-        # EmitEvent(
-        #     event = ChangeState(
-        #     lifecycle_node_matcher = launch.events.process.matches_name('global_costmap'),
-        #     transition_id = lifecycle_msgs.msg.Transition.TRANSITION_CONFIGURE
-        #     )
-        # ),
+        ## Global map server configure and activated
+        ExecuteProcess(
+            cmd=[[
+                FindExecutable(name='ros2'),
+                " service call ",
+                "/global_map_server/change_state ",
+                "lifecycle_msgs/srv/ChangeState ",
+                '"{transition: {id: 1}}"',
+                '&&',
+                FindExecutable(name='ros2'),
+                " service call ",
+                "/global_map_server/change_state ",
+                "lifecycle_msgs/srv/ChangeState ",
+                '"{transition: {id: 3}}"',
+            ]],
+            shell=True
+        ),  
 
-        #     # Make the ZED node take the 'activate' transition
-        # EmitEvent(
-        #     event = ChangeState(
-        #     lifecycle_node_matcher = launch.events.process.matches_name('global_map_server'),
-        #     transition_id = lifecycle_msgs.msg.Transition.TRANSITION_ACTIVATE,
-        #     )
-        # ),
 
-        # EmitEvent(
-        #     event = ChangeState(
-        #     lifecycle_node_matcher = launch.events.process.matches_name('global_costmap'),
-        #     transition_id = lifecycle_msgs.msg.Transition.TRANSITION_ACTIVATE
-        #     )
-        # )
-
-        Node(
-            package='nav2_lifecycle_manager',
-            executable='lifecycle_manager',
-            name='lifecycle_manager_global_map',
-            output='screen',
-            arguments=['--ros-args', '--log-level', log_level],
-            parameters=[{'use_sim_time': use_sim_time},
-                        {'autostart': autostart},
-                        {'node_names': ['global_map_server','costmap/costmap']}]),
+        ## Global costmap configure and activated
+        ExecuteProcess(
+            cmd=[[
+                FindExecutable(name='ros2'),
+                " service call ",
+                "/costmap/costmap/change_state ",
+                "lifecycle_msgs/srv/ChangeState ",
+                '"{transition: {id: 1}}"',
+                '&&',
+                FindExecutable(name='ros2'),
+                " service call ",
+                "/costmap/costmap/change_state ",
+                "lifecycle_msgs/srv/ChangeState ",
+                '"{transition: {id: 3}}"',
+            ]],
+            shell=True
+        ),  
     ])
     
