7b3e246bad3d99442d7198c42348da6a2963e361
Olibeyer <olauri18@student.aau.dk>
2023-03-27 15:31:00 +0200

    Merge branch 'main' of https://github.com/Anders-Clement/P10

--- src/spice/launch/robot_bringup.launch.py
+++ src/spice/launch/robot_bringup.launch.py
@@ -19,7 +19,7 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
 
-DEFAULT_MAP_NAME = 'C4.yaml' # change to the name.yaml of the default map here
+DEFAULT_MAP_NAME = 'canteen.yaml' # change to the name.yaml of the default map here
 
 def generate_launch_description():
     map_name = LaunchConfiguration('map')
