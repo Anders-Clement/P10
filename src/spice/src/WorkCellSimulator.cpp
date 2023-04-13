f82a0bf618399d9bd0dfbc45517dcb1142488f31
Olibeyer <olauri18@student.aau.dk>
2023-04-13 10:58:12 +0200

    A4 poses and config

--- src/spice/src/WorkCellSimulator.cpp
+++ src/spice/src/WorkCellSimulator.cpp
@@ -21,55 +21,89 @@ public:
 
     geometry_msgs::msg::Transform C4Locations(){
 
-        geometry_msgs::msg::Transform poses[6];
-        // C4 POSES
-        poses[0].rotation.w = 0.999997;
-        poses[0].rotation.x = 0.0 ;
+        geometry_msgs::msg::Transform poses[4];
+        //A4
+        poses[0].rotation.w = 0.709132;
+        poses[0].rotation.x = 0.0;
         poses[0].rotation.y = 0.0;
-        poses[0].rotation.z = 0.00250493;
-        poses[0].translation.x = 18.7613;
-        poses[0].translation.y = 7.78589;
+        poses[0].rotation.z = 0.7050776;
+        poses[0].translation.x = 6.27535;
+        poses[0].translation.y = 0.518605;
         poses[0].translation.z = 0.0;
         
-        poses[1].rotation.w = 0.999997;
+        poses[1].rotation.w = 0.723702;
         poses[1].rotation.x = 0;
         poses[1].rotation.y = 0;
-        poses[1].rotation.z = 0.00250493;
-        poses[1].translation.x = 15.9208;
-        poses[1].translation.y = 5.6733;
+        poses[1].rotation.z = 0.690113;
+        poses[1].translation.x = 4.10358;
+        poses[1].translation.y = 0.446661;
         poses[1].translation.z = 0;
 
-        poses[2].rotation.w = 0.999997;
+        poses[2].rotation.w = 1;
         poses[2].rotation.x = 0;
         poses[2].rotation.y = 0;
-        poses[2].rotation.z = 0.00250493;
-        poses[2].translation.x = 15.9608;
-        poses[2].translation.y = 9.71498;
+        poses[2].rotation.z = 0.000737653;
+        poses[2].translation.x = 0.358586;
+        poses[2].translation.y = -1.50395;
         poses[2].translation.z = 0;
 
-        poses[3].rotation.w = 0.999997;
+        poses[3].rotation.w = 0.999716;
         poses[3].rotation.x = 0;
         poses[3].rotation.y = 0;
-        poses[3].rotation.z = 0.00250493;
-        poses[3].translation.x = 16.0792;
-        poses[3].translation.y = 12.8173;
+        poses[3].rotation.z = 0.0238133;
+        poses[3].translation.x = 0.383676;
+        poses[3].translation.y = 0.486885;
         poses[3].translation.z = 0;
+        
+        
+        // C4 POSES
+        // poses[0].rotation.w = 0.999997;
+        // poses[0].rotation.x = 0.0 ;
+        // poses[0].rotation.y = 0.0;
+        // poses[0].rotation.z = 0.00250493;
+        // poses[0].translation.x = 18.7613;
+        // poses[0].translation.y = 7.78589;
+        // poses[0].translation.z = 0.0;
+        
+        // poses[1].rotation.w = 0.999997;
+        // poses[1].rotation.x = 0;
+        // poses[1].rotation.y = 0;
+        // poses[1].rotation.z = 0.00250493;
+        // poses[1].translation.x = 15.9208;
+        // poses[1].translation.y = 5.6733;
+        // poses[1].translation.z = 0;
 
-        poses[4].rotation.w = 0.999997;
-        poses[4].rotation.x = 0;
-        poses[4].rotation.y = 0;
-        poses[4].rotation.z = 0.00250493;
-        poses[4].translation.x = 16.229;
-        poses[4].translation.y = 17.366;
-        poses[4].translation.z = 0;
+        // poses[2].rotation.w = 0.999997;
+        // poses[2].rotation.x = 0;
+        // poses[2].rotation.y = 0;
+        // poses[2].rotation.z = 0.00250493;
+        // poses[2].translation.x = 15.9608;
+        // poses[2].translation.y = 9.71498;
+        // poses[2].translation.z = 0;
 
-        poses[5].rotation.w = 1;
-        poses[5].rotation.x = 0;
-        poses[5].rotation.y = 0;
-        poses[5].rotation.z = 0;
-        poses[5].translation.x = 16.6054;
-        poses[5].translation.y = 19.8698;
-        poses[5].translation.z = 0;
+        // poses[3].rotation.w = 0.999997;
+        // poses[3].rotation.x = 0;
+        // poses[3].rotation.y = 0;
+        // poses[3].rotation.z = 0.00250493;
+        // poses[3].translation.x = 16.0792;
+        // poses[3].translation.y = 12.8173;
+        // poses[3].translation.z = 0;
+
+        // poses[4].rotation.w = 0.999997;
+        // poses[4].rotation.x = 0;
+        // poses[4].rotation.y = 0;
+        // poses[4].rotation.z = 0.00250493;
+        // poses[4].translation.x = 16.229;
+        // poses[4].translation.y = 17.366;
+        // poses[4].translation.z = 0;
+
+        // poses[5].rotation.w = 1;
+        // poses[5].rotation.x = 0;
+        // poses[5].rotation.y = 0;
+        // poses[5].rotation.z = 0;
+        // poses[5].translation.x = 16.6054;
+        // poses[5].translation.y = 19.8698;
+        // poses[5].translation.z = 0;
         // CANTEEN POSES
         // poses[0].rotation.w = 1.0;
         // poses[0].rotation.x =0.0 ;
@@ -159,11 +193,11 @@ public:
                 spice_msgs::msg::RobotType::WORK_CELL_FUSES, 
                 *this,
                 generator.C4Locations()),
-            std::make_shared<WorkCellStateMachine>(
-                "back_cover_cell_2", 
-                spice_msgs::msg::RobotType::WORK_CELL_BACK_COVER, 
-                *this,
-                generator.C4Locations()),
+            // std::make_shared<WorkCellStateMachine>(
+            //     "back_cover_cell_2", 
+            //     spice_msgs::msg::RobotType::WORK_CELL_BACK_COVER, 
+            //     *this,
+            //     generator.C4Locations()),
             
             /*std::make_shared<WorkCellStateMachine>(
                 "drill_cell_2", spice_msgs::msg::RobotType::WORK_CELL_DRILL, 
