d2cca85776f31565cbd0d976c9f7a015571a3d06
Olibeyer <olauri18@student.aau.dk>
2023-04-25 10:39:48 +0200

    wip moved queue manager to own class

---
+++ src/spice/include/spice/work_cell_queue_manager.hpp
@@ -0,0 +1,77 @@
+#ifndef WORK_CELL_QUEUE_MANAGER_HPP
+#define WORK_CELL_QUEUE_MANAGER_HPP
+
+#include <string>
+#include <rclcpp/node.hpp>
+#include <optional>
+#include <queue>
+#include "std_srvs/srv/trigger.hpp"
+#include "geometry_msgs/msg/transform_stamped.hpp"
+#include "tf2_ros/static_transform_broadcaster.h"
+#include "spice_msgs/msg/robot_type.hpp"
+#include "spice_msgs/msg/task.hpp"
+#include "spice_msgs/msg/id.hpp"
+#include "spice_msgs/msg/robot_type.hpp"
+#include "spice_msgs/msg/robot_state_transition.hpp"
+#include "spice_msgs/srv/register_work.hpp"
+#include "spice_msgs/srv/heartbeat.hpp"
+#include "spice/work_cell.hpp"
+#include "nav_msgs/msg/occupancy_grid.hpp"
+#include "nav2_costmap_2d/costmap_2d.hpp"
+#include "nav2_costmap_2d/costmap_2d_ros.hpp"
+#include "tf2_ros/transform_listener.h"
+#include "tf2_ros/buffer.h"
+#include "spice_msgs/srv/get_robots_by_type.hpp"
+#include "spice/work_cell_state_machine.hpp"
+
+
+class WorkCellQueueManager{
+    public:
+    WorkCellQueueManager() = delete;
+    WorkCellQueueManager(WorkCellStateMachine& workCellStateMachine);
+    
+    void global_costmap_cb(nav_msgs::msg::OccupancyGrid::SharedPtr msg); // check
+    void timer_update_robots_lists();//check
+    void timer_update_q_locations(); // check
+    void update_workcell_costmap();// check
+
+
+    int pnpoly(int nvert, std::vector<float> vertx, std::vector<float> verty, float testx, float testy); // check
+    void inflateCostMap(int current_loop, std::shared_ptr<nav2_costmap_2d::Costmap2D> costmap, float slope); // check
+    void publish_costmap(std::shared_ptr<nav2_costmap_2d::Costmap2D> costmap); //check
+    
+    void attraction(std::shared_ptr<nav2_costmap_2d::Costmap2D> costmap, float slope, std::pair<unsigned int, unsigned int> wc_center_point); // check
+
+    rclcpp::Logger get_logger();    
+
+    private:
+    WorkCellStateMachine& m_workCellStateMachine;
+
+
+    rclcpp::TimerBase::SharedPtr m_timer_robots_lists{nullptr};
+    rclcpp::TimerBase::SharedPtr m_timer_q{nullptr};
+    std::shared_ptr<tf2_ros::TransformListener> tf_listener_{nullptr};
+    rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr m_costmapPub;
+    std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
+    rclcpp::Client<spice_msgs::srv::GetRobotsByType>::SharedPtr get_workcells_cli;
+    rclcpp::Client<spice_msgs::srv::GetRobotsByType>::SharedPtr get_carriers_cli;
+
+
+    std::shared_ptr<nav2_costmap_2d::Costmap2D> m_global_costmap;
+    std::shared_ptr<nav2_costmap_2d::Costmap2D> workcell_costmap;
+
+    rclcpp::Subscription<nav_msgs::msg::OccupancyGrid>::SharedPtr m_costmap_subscriber;
+    std::vector<std::pair<unsigned int, unsigned int>> costpoints;
+    std::mutex m_mutex;
+    float MAX_Q_VEL = 0.30;
+    std::chrono::_V2::system_clock::time_point lastTime;
+    std::pair<unsigned int, unsigned int> map_coord_entry;
+    std::vector<std::pair<unsigned int, unsigned int>> viable_points;
+    std::vector<std::pair<float,float>> world_corners;
+    std::vector<float> world_corners_x;
+    std::vector<float> world_corners_y;
+    std::vector<spice_msgs::msg::Robot> workcell_list; //list of all workcells
+    std::vector<spice_msgs::msg::Robot> carrier_list; //list of all carrier robots
+};
+
+#endif
--- src/spice/include/spice/work_cell_state_machine.hpp
+++ src/spice/include/spice/work_cell_state_machine.hpp
@@ -22,6 +22,7 @@
 #include "tf2_ros/transform_listener.h"
 #include "tf2_ros/buffer.h"
 #include "spice_msgs/srv/get_robots_by_type.hpp"
+#include "spice/work_cell_queue_manager.hpp"
 
 enum class WORK_CELL_STATE : uint8_t{
     STARTUP = 0,
@@ -60,8 +61,14 @@ public:
     std::shared_ptr<rclcpp::Client<std_srvs::srv::Trigger>> m_call_robot_client;
     std::shared_ptr<rclcpp::Client<std_srvs::srv::Trigger>> m_done_processing_client;
     spice_msgs::srv::RegisterWork::Request m_current_robot_work;
-private:
+    std::string m_work_cell_name;
+    geometry_msgs::msg::Transform m_transform;
+    geometry_msgs::msg::Transform m_entry_transform;
+    geometry_msgs::msg::Transform m_exit_transform;
+    std::vector<geometry_msgs::msg::Transform> m_q_transforms;
+    int m_q_num = 3;
 
+private:
     void on_register_robot(
         const std::shared_ptr<spice_msgs::srv::RegisterWork::Request> request, 
         std::shared_ptr<spice_msgs::srv::RegisterWork::Response> response);
@@ -69,39 +76,13 @@ private:
         const std::shared_ptr<std_srvs::srv::Trigger::Request> request, 
         std::shared_ptr<std_srvs::srv::Trigger::Response> response);
     spice_msgs::msg::RobotState internal_state_to_robot_state(WORK_CELL_STATE state);
+    std::unique_ptr<WorkCellQueueManager> m_work_cell_queue_manager;
 
-    void timer_update_q_locations();
-    void timer_update_robots_lists();
-    void global_costmap_cb(nav_msgs::msg::OccupancyGrid::SharedPtr msg);
-    int pnpoly(int nvert, std::vector<float> vertx, std::vector<float> verty, float testx, float testy);
-    void inflateCostMap(int current_loop, std::shared_ptr<nav2_costmap_2d::Costmap2D> costmap, float slope);
-    void publish_costmap(std::shared_ptr<nav2_costmap_2d::Costmap2D> costmap);
-    void attraction(std::shared_ptr<nav2_costmap_2d::Costmap2D> costmap, float slope, std::pair<unsigned int, unsigned int> wc_center_point);
-    void update_workcell_costmap();
 
-    std::string m_work_cell_name;
     spice_msgs::msg::RobotType::_type_type m_robot_type;
-    geometry_msgs::msg::Transform m_transform;
-    geometry_msgs::msg::Transform m_entry_transform;
-    geometry_msgs::msg::Transform m_exit_transform;
-    std::vector<geometry_msgs::msg::Transform> m_q_transforms;
+
     double ROBOT_RADIUS = 0.25;
     double WORKCELL_RADIUS = 0.25;
-    int q_num = 3;
-    std::pair<unsigned int, unsigned int> map_coord_entry;
-    std::vector<std::pair<unsigned int, unsigned int>> viable_points;
-    rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr m_costmapPub;
-    rclcpp::TimerBase::SharedPtr m_timer_robots_lists{nullptr};
-    rclcpp::TimerBase::SharedPtr m_timer_q{nullptr};
-    std::vector<spice_msgs::msg::Robot> workcell_list; //list of all workcells
-    std::vector<spice_msgs::msg::Robot> carrier_list; //list of all carrier robots
-    rclcpp::Client<spice_msgs::srv::GetRobotsByType>::SharedPtr get_workcells_cli;
-    rclcpp::Client<spice_msgs::srv::GetRobotsByType>::SharedPtr get_carriers_cli;
-    std::shared_ptr<tf2_ros::TransformListener> tf_listener_{nullptr};
-    std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
-    std::vector<std::pair<float,float>> world_corners;
-    std::vector<float> world_corners_x;
-    std::vector<float> world_corners_y;
     std::vector<std::shared_ptr<WorkCellState>> m_states;
     WORK_CELL_STATE m_current_state;
     std::shared_ptr<tf2_ros::StaticTransformBroadcaster> m_tf_static_broadcaster;
@@ -111,14 +92,7 @@ private:
     std::shared_ptr<rclcpp::Service<std_srvs::srv::Trigger>> m_robot_ready_for_processing_service;
     std::shared_ptr<rclcpp::Publisher<spice_msgs::msg::RobotStateTransition>> m_state_transition_event_pub;
     std::queue<spice_msgs::srv::RegisterWork::Request> m_enqueued_robots;
-    std::shared_ptr<nav2_costmap_2d::Costmap2D> m_global_costmap;
-    std::shared_ptr<nav2_costmap_2d::Costmap2D> workcell_costmap;
 
-    rclcpp::Subscription<nav_msgs::msg::OccupancyGrid>::SharedPtr m_costmap_subscriber;
-    std::vector<std::pair<unsigned int, unsigned int>> costpoints;
-    std::mutex m_mutex;
-    float MAX_Q_VEL = 0.30;
-    std::chrono::_V2::system_clock::time_point lastTime;
     
 };
 
