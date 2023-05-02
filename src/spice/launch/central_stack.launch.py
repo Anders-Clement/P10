0571e6a11d1a6bf58f133a0ee4e538fcf8cdedb6
Jonas Andersen <jan18@student.aau.dk>
2023-05-02 13:51:41 +0200

    Added cost at exit and changed map to A4

--- src/spice/launch/central_stack.launch.py
+++ src/spice/launch/central_stack.launch.py
@@ -6,7 +6,7 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration
 
-DEFAULT_MAP_NAME = "C4.yaml" # change to the name.yaml of the default map here
+DEFAULT_MAP_NAME = "A4.yaml" # change to the name.yaml of the default map here
 
 def generate_launch_description():
 
@@ -41,17 +41,17 @@ def generate_launch_description():
             package='spice',
             executable='work_cell_simulator',
             name='work_cell_simulator',
-            parameters=[{'work_cell_rep_slope': 0.05}, # workcell repulsion for queue pos
-                        {'carrier_bot_rep_slope': 0.1}, # carrier bot repulsion
-                        {'wall_rep_slope': 0.1}, #static map obstacle repulsion
-                        {'queue_rep_slope': 0.1}, # other queue point repulsion
-                        {'plan_rep_slope': 0.1}, # robot plans repulsion
-                        {'work_cell_att_slope':  0.05}, # own workcell attraction
-                        {'queue_att_slope': 0.0}, #own lower queue point attraction
-                        {'min_move_dist': 5},
-                        {'q_max_vel': 0.33},
-                        {'map': map_name}
-
+            parameters=[
+                {'work_cell_rep_slope': 0.05}, # workcell repulsion for queue pos
+                {'carrier_bot_rep_slope': 0.1}, # carrier bot repulsion
+                {'wall_rep_slope': 0.1}, #static map obstacle repulsion
+                {'queue_rep_slope': 0.1}, # other queue point repulsion
+                {'plan_rep_slope': 0.1}, # robot plans repulsion
+                {'work_cell_att_slope':  0.05}, # own workcell attraction
+                {'queue_att_slope': 0.0}, #own lower queue point attraction
+                {'min_move_dist': 5},
+                {'q_max_vel': 0.33},
+                {'map': map_name}
             ]
         ),
 
@@ -59,9 +59,10 @@ def generate_launch_description():
             package='spice',
             executable='central_path_planner',
             name='central_path_planner',
-            parameters=[{'priority_scheme': 0}, # 0 static priorites, 1 shortest dist hihgest priority, 2 planning failed gives highest priority 
-                        {'future_lookup': 0}, # number of higer priority plan steps to consider when planning 
-                        {'cost': 254} # cost of other higher priority robots' plans 
+            parameters=[
+                {'priority_scheme': 0}, # 0 static priorites, 1 shortest dist hihgest priority, 2 planning failed gives highest priority 
+                {'future_lookup': 0}, # number of higer priority plan steps to consider when planning 
+                {'cost': 254} # cost of other higher priority robots' plans 
             ]
         ),
 
