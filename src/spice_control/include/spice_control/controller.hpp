ee76b27cf3ea3e9916a326c99491b95f6482906f
Anders Clement <andersclement96@gmail.com>
2023-05-09 09:59:14 +0200

    Straight line controller

---
+++ src/spice_control/include/spice_control/controller.hpp
@@ -0,0 +1,82 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ *  Author(s): Shrijit Singh <shrijitsingh99@gmail.com>
+ *
+ */
+
+#ifndef SPICE_CONTROLLER_HPP
+#define SPICE_CONTROLLER_HPP
+
+#include <string>
+#include <vector>
+#include <memory>
+
+#include "nav2_core/controller.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "pluginlib/class_loader.hpp"
+#include "pluginlib/class_list_macros.hpp"
+
+namespace nav2_pure_pursuit_controller
+{
+
+class SpiceController : public nav2_core::Controller
+{
+public:
+  SpiceController() = default;
+  ~SpiceController() override = default;
+
+  void configure(
+    const rclcpp_lifecycle::LifecycleNode::WeakPtr & parent,
+    std::string name, const std::shared_ptr<tf2_ros::Buffer>tf,
+    const std::shared_ptr<nav2_costmap_2d::Costmap2DROS>costmap_ros) override;
+
+
+  void cleanup() override;
+  void activate() override;
+  void deactivate() override;
+
+  geometry_msgs::msg::TwistStamped computeVelocityCommands(
+    const geometry_msgs::msg::PoseStamped & pose,
+    const geometry_msgs::msg::Twist & velocity,
+    nav2_core::GoalChecker * goal_checker) override;
+
+  void setPlan(const nav_msgs::msg::Path & path) override;
+  void setSpeedLimit(const double & speed_limit, const bool & percentage) override;
+
+protected:
+  nav_msgs::msg::Path transformGlobalPlan(const geometry_msgs::msg::PoseStamped & pose);
+  void get_params();
+
+  bool transformPose(
+    const std::shared_ptr<tf2_ros::Buffer> tf,
+    const std::string frame,
+    const geometry_msgs::msg::PoseStamped & in_pose,
+    geometry_msgs::msg::PoseStamped & out_pose,
+    const rclcpp::Duration & transform_tolerance
+  ) const;
+
+  rclcpp_lifecycle::LifecycleNode::WeakPtr node_;
+  std::shared_ptr<tf2_ros::Buffer> tf_;
+  std::string plugin_name_;
+  std::shared_ptr<nav2_costmap_2d::Costmap2DROS> costmap_ros_;
+  rclcpp::Logger logger_ {rclcpp::get_logger("SpiceController")};
+  rclcpp::Clock::SharedPtr clock_;
+
+  double desired_linear_vel_;
+  double min_linear_vel_;
+  double min_angular_vel_;
+  double gamma_max_;
+  double max_angular_vel_;
+  double kp_omega_;
+  double kp_linear_vel_;
+  double goal_tolerance_;
+  rclcpp::Duration transform_tolerance_ {0, 0};
+
+  nav_msgs::msg::Path global_plan_;
+  std::shared_ptr<rclcpp_lifecycle::LifecyclePublisher<nav_msgs::msg::Path>> global_pub_;
+};
+
+}  // namespace nav2_pure_pursuit_controller
+
+#endif  // SPICE_CONTROLLER_HPP
