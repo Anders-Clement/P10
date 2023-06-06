23ca2181e156487e09bbb1bc17c40d79e12e1577
Olibeyer <olauri18@student.aau.dk>
2023-06-06 12:10:14 +0200

    moving workstations

--- src/spice/include/spice/work_cells/work_cell_state_machine.hpp
+++ src/spice/include/spice/work_cells/work_cell_state_machine.hpp
@@ -115,6 +115,9 @@ private:
         const std::shared_ptr<spice_msgs::srv::Heartbeat::Request> request,
         std::shared_ptr<spice_msgs::srv::Heartbeat::Response> response);
     void check_robot_heartbeat_cb();
+    void nav_goal_cb(const std::shared_ptr<geometry_msgs::msg::PoseStamped> msg);
+    void move_work_cell();
+
     spice_msgs::msg::RobotState internal_state_to_robot_state(WORK_CELL_STATE state);
     std::unique_ptr<WorkCellQueuePositionManager> m_work_cell_queue_manager;
 
@@ -143,9 +146,12 @@ private:
     std::shared_ptr<rclcpp::Publisher<spice_msgs::msg::RobotStateTransition>> m_state_transition_event_pub;
     rclcpp::TimerBase::SharedPtr m_timer{nullptr};
     rclcpp::TimerBase::SharedPtr m_robot_heartbeat_timer;
+    rclcpp::TimerBase::SharedPtr m_nav_goal_timer;
     std::shared_ptr<nav2_costmap_2d::Costmap2D> m_global_costmap;
     rclcpp::Subscription<nav_msgs::msg::OccupancyGrid>::SharedPtr m_costmap_subscriber;
-    
+    bool prepare_move = false;
+    std::shared_ptr<rclcpp::Subscription<geometry_msgs::msg::PoseStamped>> m_nav_goal_subscriber;
+    geometry_msgs::msg::TransformStamped goal_point;
 };
 
 #endif
