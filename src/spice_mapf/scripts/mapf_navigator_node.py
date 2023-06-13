74aaf117df81af82194188e4b987eac53738b52b
Anders Clement <andersclement96@gmail.com>
2023-06-13 10:49:30 +0200

    .

--- src/spice_mapf/scripts/mapf_navigator_node.py
+++ src/spice_mapf/scripts/mapf_navigator_node.py
@@ -46,7 +46,7 @@ class MAPFNavigator(Node):
         self.paths_subscriber = self.create_subscription(spice_mapf_msgs.RobotPoses, "/mapf_paths", self.paths_cb, 10)
         self.cmd_vel_publisher = self.create_publisher(Twist, "cmd_vel", 10)
 
-        self.tf_buffer = tf2_ros.buffer.Buffer(Duration(seconds=1, nanoseconds=0))
+        self.tf_buffer = tf2_ros.buffer.Buffer(Duration(seconds=1))
         self.tf_listener = tf2_ros.transform_listener.TransformListener(self.tf_buffer, self)
 
         self.controller = mapf_controller.MAPFController(self, self.tf_buffer)
@@ -154,7 +154,7 @@ class MAPFNavigator(Node):
         try:
             from_frame_rel = "base_link"
             to_frame_rel = "map"
-            self.current_transform = self.tf_buffer.lookup_transform(to_frame_rel, from_frame_rel, 0)
+            self.current_transform = self.tf_buffer.lookup_transform(to_frame_rel, from_frame_rel, Time())
             return True
         except TransformException as e:
             self.get_logger().info(
